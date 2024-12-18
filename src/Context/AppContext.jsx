9da48745e658878328e2e543f0761fadba4b045b
Esra <esraayaareb@gmail.com>
2024-12-18 23:53:34 +0300

    remove if 0

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -24,9 +24,7 @@ export default function AppProvider({ children }) {
         case "subtract":
           return prevItems
             .map((item) =>
-              item.id === id && item.quantity > 1
-                ? { ...item, quantity: item.quantity - 1 }
-                : item
+              item.id === id ? { ...item, quantity: item.quantity - 1 } : item
             )
             .filter((item) => item.quantity > 0);
 
